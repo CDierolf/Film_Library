bdf6d0d53850c04fae4520565a49231690d25f3d
Christopher Dierolf <chidi117@gmail.com>
2018-09-02 11:12:45 -0500

    Added the movieAPI project. Added Newtonsoft Json references.

---
+++ MovieAPI/Program.cs
@@ -0,0 +1,48 @@
+﻿using MovieAPI.models;
+using MovieAPI.viewmodels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetMovieTitle();
+            Console.ReadLine();
+        }
+
+        public static void GetMovieTitle()
+        {
+
+            int cont = 0;
+            string movieTitle;
+            do
+            {
+                Console.Write("Enter a movie title to search for: ");
+                movieTitle = Console.ReadLine();
+                GetMovieData(movieTitle);
+                Console.ReadLine();
+            } while (cont != 0);
+        }
+
+        public static async void GetMovieData(string movieTitle)
+        {
+            MovieDataAPIVM movieVM = new MovieDataAPIVM();
+            MovieData movieData = new MovieData();
+
+            movieData = await movieVM.GetMovieDataAsync(movieTitle);
+
+            Console.WriteLine("Here are your results:\n " +
+                "Title: {0} \n Year Released: {1} \n Genre: {2} \n Writer: {3} \n Plot: {4} ",
+                movieData.Title, movieData.Year, movieData.Genre, movieData.Writer, movieData.Plot);
+
+        }
+    }
+}
---
+++ MovieAPI/models/MovieAPI.cs
@@ -0,0 +1,48 @@
+﻿using MovieAPI.models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MovieAPI.viewmodels
+{
+    class MovieDataAPIVM
+    {
+        public const string API_KEY = "49bee854";
+        public const string BASE_URL = "http://www.omdbapi.com/?t={0}&apikey={1}"; // 0 = movie title, 1 = API_KEY
+
+        /// <summary>
+        /// Async task of MovieData
+        /// Runs on a different thread so it doesn't bog down the app
+        /// while awaiting a response and parsing the json data.
+        /// Accepts a movie title as a parameter to search for.
+        /// </summary>
+        /// <param name="movieTitle"></param>
+        /// <returns></returns>
+        public async Task<MovieData> GetMovieDataAsync(string movieTitle)
+        {
+            MovieData result = new MovieData(); // Create a new MovieData object
+
+            string url = string.Format(BASE_URL, movieTitle, API_KEY); // Modify the base url
+
+            using (HttpClient client = new HttpClient()) // Create and use an HttpClient object
+            {
+                var response = await client.GetAsync(url);
+
+                string json = await response.Content.ReadAsStringAsync();
+
+                /* Deserialize the json data using Newtonsoft.Json's JsonConvert method.
+                 * Creates a MovieData object out of the json 
+                 */
+                result = JsonConvert.DeserializeObject<MovieData>(json);
+            }
+            return result;
+        }
+    }
+
+
+}
