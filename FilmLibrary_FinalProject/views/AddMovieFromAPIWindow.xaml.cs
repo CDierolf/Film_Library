a4acbd60a6af3c7c96c58db418f451e22c215c1d
Christopher Dierolf <chidi117@gmail.com>
2018-11-09 12:35:19 -0600

    Plugged API project into the AddMovieAPIWindow. Movie data currently appears in a messagebox.

--- FilmLibrary_FinalProject/views/AddMovieFromAPIWindow.xaml.cs
+++ FilmLibrary_FinalProject/views/AddMovieFromAPIWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieAPI.models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,11 +23,42 @@ namespace FilmLibrary_FinalProject.views
         public AddMovieFromAPIWindow()
         {
             InitializeComponent();
+
+            txtAPIMovieTitle.Text = null;
         }
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        public void GetMovieTitle()
         {
+            int cont = 0;
+            string movieTitle, formattedTitle;
+            do
+            {
+                Console.Write("Enter a movie title to search for: ");
+                movieTitle = Console.ReadLine();
+                formattedTitle = movieTitle.Replace(" ", "_"); // Replace spaces with underscores for more accurate results.
+                GetMovieData(movieTitle);
+                Console.ReadLine();
+            } while (cont != 0);
+        }
 
+        public static async void GetMovieData(string movieTitle)
+        {
+            MovieAPIModel movieVM = new MovieAPIModel();
+            MovieData movieData = new MovieData();
+
+            movieData = await movieVM.GetMovieDataAsync(movieTitle);
+
+            MessageBox.Show("Here are your results:\n " +
+                "Title: " + movieData.Title + " \n Year Released: " + movieData.Year + " \n Genre: " + movieData.Genre +
+                " \n Writer: " + movieData.Writer + " \n Plot: " + movieData.Plot);
+        }
+
+        private void btAPISearch_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtAPIMovieTitle != null)
+                GetMovieData(txtAPIMovieTitle.Text);
+            else
+                MessageBox.Show("Movie title cannot be blank");
         }
     }
 }
