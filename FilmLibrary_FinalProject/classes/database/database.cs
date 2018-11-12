3c795b2acd15dd623f69525865b17867022af667
Christopher Dierolf <chidi117@gmail.com>
2018-11-12 11:54:12 -0600

    Spelling error

--- FilmLibrary_FinalProject/classes/database/database.cs
+++ FilmLibrary_FinalProject/classes/database/database.cs
@@ -11,7 +11,7 @@ namespace FilmLibraryDatabase
 {
     public class DBConnectionClass
     {
-        // Coments
+        // Comments
         static string dbName = "FilmDatabase.db";
         static string dbFolderPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + dbName;
         static string dbPath = String.Format(dbFolderPath, dbName);
